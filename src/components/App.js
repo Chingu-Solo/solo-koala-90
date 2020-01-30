026364fa92b2ecafe6c4f6e112ddcea8c4647b00
jenny07007 <jennyliu07@gmail.com>
2020-01-31 01:05:00 +0800

    create new branches and work on searchBar feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import SearchBar from './SearchBar'
 import "./app.css";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
     </div>
   );
 }
